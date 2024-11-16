940391b817939471706bf86f87ae7a4f0e7e8b3c
modkasimi <40761363+modkasimi@users.noreply.github.com>
2024-11-16 23:01:08 +0100

    Update detectLang.js

--- detectLang.js
+++ detectLang.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
             ar: "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F100085252551188%2Fvideos%2F876555391335228%2F&show_text=false&width=560&t=0",
             
         },
-        returnRepair: {
+        returnRepairs: {
             en: "https://www.facebook.com/plugins/video.php?href=ENGLISH_VIDEO_URL_FOR_PAGE2",
             fr: "https://www.facebook.com/plugins/video.php?href=FRENCH_VIDEO_URL_FOR_PAGE2",
             ar: "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F100085252551188%2Fvideos%2F575969954945459%2F&show_text=false&width=560&t=0" 

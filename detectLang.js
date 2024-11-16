5f10e85965805e2a09bbcc8d1f658f6de1127e9b
modkasimi <40761363+modkasimi@users.noreply.github.com>
2024-11-16 18:16:14 +0100

    Update detectLang.js

--- detectLang.js
+++ detectLang.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Define videos for each page and language
     const videos = {
         cancelOperation: {
-            en: "https://www.facebook.com/plugins/video.php?href=ARABIC_VIDEO_URL_FOR_PAGE1"
+            en: "https://www.facebook.com/plugins/video.php?href=ARABIC_VIDEO_URL_FOR_PAGE1",
             fr: "https://www.facebook.com/plugins/video.php?href=FRENCH_VIDEO_URL_FOR_PAGE1",
             ar: "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F100085252551188%2Fvideos%2F876555391335228%2F&show_text=false&width=560&t=0",
             
